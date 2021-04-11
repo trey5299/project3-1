a0e051d7252639973c2113312b9cd3eae34952f5
trey5299 <61715414+trey5299@users.noreply.github.com>
2021-04-11 14:26:23 -0400

    Add files via upload

---
+++ test_project3.py
@@ -0,0 +1,44 @@
+def test_phonecall():
+    customer = "4042592350"
+    phonecall = customer
+    assert phonecall
+
+def test_phonebill():
+    customer = "John"
+    phonebill = customer
+    assert phonebill
+
+def test_starttime():
+    starttime = "02:23:09"
+    phonecall = starttime
+    assert starttime
+
+def test_startdate():
+    startdate = "07/03/2021"
+    phonecall = startdate
+    assert startdate
+
+def test_enddate():
+    enddate = "07/05/2021"
+    phonecall = enddate
+    assert enddate
+
+def test_caller():
+    caller = "Sarah"
+    phonecall = caller
+    assert caller
+
+def test_callee():
+    callee = "Luke"
+    phonecall = callee
+    assert callee
+
+def test_get_customername():
+    customername = "Mark"
+    phonebill = customername
+    assert customername
+
+
+
+
+
